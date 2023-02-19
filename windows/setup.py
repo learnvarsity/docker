dcf6355eb352bff4b2bb138022b255a9c3e1027d
Raj Gopinath <125770743+learnvarsity@users.noreply.github.com>
2023-02-19 10:12:35 -0600

    Update setup.py

--- windows/setup.py
+++ windows/setup.py
@@ -1,5 +1,24 @@
 
-#
-Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
+# run docker on windows
 
-Restart-Computer -Force
+# open power shell and run the following command lines
+
+
+# first time installation
+
+# Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
+# Install-Package -Name docker -ProviderName DockerMsftProvider
+# Restart-Computer -Force
+# Get-Package -Name Docker -ProviderName DockerMsftProvider
+
+
+# steps docker for future docker software uprades
+
+# Install-Package -Name Docker -ProviderName DockerMsftProvider -Update -Force
+# Start-Service Docker
+
+
+
+# references
+
+# original link - https://computingforgeeks.com/how-to-run-docker-containers-on-windows-server-2019/
